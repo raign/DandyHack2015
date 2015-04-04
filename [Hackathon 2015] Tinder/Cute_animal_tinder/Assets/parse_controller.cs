c3ee1d194e65832746f1bda3310f680c4d4cefd1
awsk1994 <awsk1994@gmail.com>
2015-04-04 17:10:29 -0400

    Unity3D File ver.1.3

--- [Hackathon 2015] Tinder/Cute_animal_tinder/Assets/parse_controller.cs
+++ [Hackathon 2015] Tinder/Cute_animal_tinder/Assets/parse_controller.cs
@@ -5,13 +5,22 @@ using Parse;
 using System.Collections.Generic;
 
 
-public class parse_controller: MonoBehaviour {
 
+public class parse_controller: MonoBehaviour {
+	Texture2D asdf;
+	int img_num =0;
 	ParseObject test;
 	string score = "";
+	int num;
+	string s;
+	int count=0;
+	string[] id = new string[4];
+	bool next = false;
+	public GUIStyle sample;
 
 	// Use this for initialization
-	void Start () {
+	void getString() {
+		//LoadFile ();
 
 		/*
 		//upload string/data types
@@ -38,12 +47,17 @@ public class parse_controller: MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-
+		if (Input.GetKeyDown("down")) {
+			//StopCoroutine(loadFile ());
+			next = true;
+			count++;
+			Debug.Log ("down: " + count);
+			//StartCoroutine (loadFile());
+		}
 	}
-
+	
 	void getParseResults()
 	{
-
 		Debug.Log ("dd");
 
 		var bigObject = new ParseObject("BigObject");
@@ -55,10 +69,55 @@ public class parse_controller: MonoBehaviour {
 		query.GetAsync("D3pswTQZJN").ContinueWith(t =>
 		                                        {
 			ParseObject bigObject2 = t.Result;
-			int number = bigObject2.Get<int>("myNumber");
-			string str = bigObject2.Get<string>("myString");
-			Debug.Log ("Number: " + number);
-			Debug.Log ("String: " + str);
+			num = bigObject2.Get<int>("myNumber");
+			s = bigObject2.Get<string>("myString");
+			Debug.Log ("Number: " + num);
+			Debug.Log ("String: " + s);
 		});
 	}
+	
+	void Awake()
+	{
+		id [1] = "bOL9HnLMqn";
+		id [2] = "j1HBJC9b4T";
+	}
+
+
+	void Start()   //single method downloads file
+	{
+		StartCoroutine (loadFile ());
+	}
+	
+	IEnumerator loadFile()
+	{
+			ParseQuery<ParseObject> query = ParseObject.GetQuery("TestObject" ).WhereEqualTo( "objectId" , id[1]);
+			
+			var queryTask = query.FirstAsync();
+			
+			while (!queryTask.IsCompleted) yield return null;
+			
+			ParseObject obj = queryTask.Result;
+			ParseFile pfile = obj.Get<ParseFile>("Image" );
+			Debug.Log (pfile.Url.AbsoluteUri);
+			var imageRequest = new WWW(pfile.Url.AbsoluteUri);
+			yield return imageRequest;   
+			Debug.Log ("imageRequest " + imageRequest.text);
+			//renderer.material.mainTexture = imageRequest.texture;
+			renderer.material.mainTexture = imageRequest.texture;
+			
+			asdf = imageRequest.texture;
+
+			yield return null;
+	}
+
+	void OnGUI()
+	{
+
+		if (asdf != null) {
+			sample.normal.background = asdf;
+		}
+		GUI.Box (new Rect (Screen.width / 4, Screen.height / 4, Screen.width / 2, Screen.height / 2), "asdf", sample);
+
+	}
+
 }
