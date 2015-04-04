da68f7b40fecbf01c2c0b2a560c54378506be7e2
raign <jan2@u.rochester.edu>
2015-04-04 19:41:36 -0400

    extreme makeover

--- website/index.php
+++ website/index.php
@@ -15,41 +15,42 @@ $path = "img/";
 	<head>
     	<script src="swipe.js"></script>
     
+    	<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+
+		<link rel="stylesheet" href="css/bootstrap.min.css">
 
 		<title>CAT: Cute Animal Tinder</title>
-
-		<link rel="stylesheet" type="text/css" href="css/boostratp.min.css">
     </head>
 
     <body>
-    	<div class="container">
-			<h1>CAT: League of Dick Pics</h1>
-
-<?php
-			echo $q;
-?>
-			<a href="#" onclick="return getOutput();"> test </a>
-			<div id="output">waiting for action</div>
-
+		<nav class="navbar navbar-inverse navbar-static-top">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">CAT: League of Dick Pics</a>
+		    </div>
+		  </div><!-- /.container-fluid -->
+		</nav>
+		<div class="container">
 			<div id="swipeBox" ontouchstart="touchStart(event,'swipeBox');" ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);">
 <?php
-	try {
-			echo '<p>', 'Database pull (TINDER) as of ', date("Y-m-d H:i:s"), '</p>';
-			$rand_id = mt_rand(1, 32);
-			$sql = "SELECT * FROM images WHERE id = '$rand_id';";
-			$rand_id_next = mt_rand(1,100);
-			echo "query = " . $sql;
-			echo "</br>";
-			foreach ($db->query($sql) as $row) {
-				//echo $path.$id.'.jpg';
-				echo "<img id = 'image_shown' data-num=$rand_id src='$path$rand_id.jpg' height='75%' width='75%'>";
-			}
-		} catch(PDOException $e) {
-			print "Error!: " . $e->getMessage() . "<br/>";
-			die();
-		}
+				try {
+					echo '<p>', 'Database pull (TINDER) as of ', date("Y-m-d H:i:s"), '</p>';
+					$rand_id = mt_rand(1, 100);
+					$sql = "SELECT * FROM images WHERE id = '$rand_id';";
+					$rand_id_next = mt_rand(1,100);
+					echo "query = " . $sql;
+					echo "</br>";
+					foreach ($db->query($sql) as $row) {
+						//echo $path.$id.'.jpg';
+						echo "<img id = 'image_shown' data-num=$rand_id src='$path$rand_id.jpg' height='300px' width='300px'>";
+					}
+				} catch(PDOException $e) {
+					print "Error!: " . $e->getMessage() . "<br/>";
+					die();
+				}
 ?>
 			</div>
 		</div>
