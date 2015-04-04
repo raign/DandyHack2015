e889d13002a8996865af20761a1905945fd1e20c
raign <jan2@u.rochester.edu>
2015-04-04 17:36:13 -0400

    cosmetic code changes
    testarinos pls

--- website/index.php
+++ website/index.php
@@ -5,62 +5,61 @@
  * Modified by: Hans Koomen
  * Modified on: 2015-02-16
  */
- session_start();
- $var_val = 'Test';
- $_SESSION['varname'] = $var_val;
- $_SESSION['table'];
- $_SESSION['email'];
- $_SESSION['time_ordered'];
- $q = $_GET['name'];
- $path = "img/";
+require_once 'dbsetup.php';
+session_start();
+$var_val = 'Test';
+$_SESSION['varname'] = $var_val;
+$_SESSION['table'];
+$_SESSION['email'];
+$_SESSION['time_ordered'];
+$q = $_GET['name'];
+$path = "../img/";
+
 ?>
 <!DOCTYPE html>
-<html><head>
-    <script src="swipe.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.2.min.js"></script>
+<html lang="en">
+	<head>
+    	<script src="swipe.js"></script>
     
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="Author" content="Dr. Hans Koomen">
-	<title>URCS DB SYS - Project Teams</title>
-	<link rel="stylesheet" type="text/css" href="hans_style.css">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<title>CAT: Cute Animal Tinder</title>
+
+		<link rel="stylesheet" type="text/css" href="css/boostratp.min.css">
     </head>
 
     <body>
-    
-	<h2>TU03 order_history</h2>
+    	<div class="container">
+			<h1>CAT: League of Dick Pics</h1>
 
-	<?php
-		require_once 'dbsetup.php';
-		//include 'response.php';
-		
-echo $q;
-    ?>
-    
-<a href="#" onclick="return getOutput();"> test </a>
-<div id="output">waiting for action</div>
+<?php
+			echo $q;
+?>
+			<a href="#" onclick="return getOutput();"> test </a>
+			<div id="output">waiting for action</div>
 
-<div id="swipeBox" ontouchstart="touchStart(event,'swipeBox');" ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);">
+			<div id="swipeBox" ontouchstart="touchStart(event,'swipeBox');" ontouchend="touchEnd(event);" ontouchmove="touchMove(event);" ontouchcancel="touchCancel(event);">
 <?php
 	try {
-	    echo '<p>', 'Database pull (TINDER) as of ', date("Y-m-d H:i:s"), '</p>';
-
-		
-		$rand_id = mt_rand(1, 32);
-	    $sql = "select * from images where id = '$rand_id';";
-		$rand_id_next = mt_rand(1,33);
-		echo "query = " . $sql;
-		echo "</br>";
-	    foreach ($db->query($sql) as $row)
-	    {		
-			//echo $path.$id.'.jpg';
-			echo "<img id = 'image_shown' data-num=$rand_id src='$path$rand_id.jpg' height='800' width='800'>";
+			echo '<p>', 'Database pull (TINDER) as of ', date("Y-m-d H:i:s"), '</p>';
+			$rand_id = mt_rand(1, 32);
+			$sql = "select * from images where id = '$rand_id';";
+			$rand_id_next = mt_rand(1,33);
+			echo "query = " . $sql;
+			echo "</br>";
+			foreach ($db->query($sql) as $row) {
+				//echo $path.$id.'.jpg';
+				echo "<img id = 'image_shown' data-num=$rand_id src='$path$rand_id.jpg' height='800' width='800'>";
 			}
-		}
-		catch (PDOException $e) {
+		} catch(PDOException $e) {
 			print "Error!: " . $e->getMessage() . "<br/>";
 			die();
 		}
-		?>
-        </div>
+?>
+			</div>
+		</div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
